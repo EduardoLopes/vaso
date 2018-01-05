8aa52a306414ffbd009a20dbc113c8c625eb76de
Eduardo Lopes <EduardoLopesDS@gmail.com>
2018-01-05 16:01:24 -0300

    Can build a bundle with especified files

--- index.js
+++ index.js
@@ -5,29 +5,73 @@ const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 const gutil = require('gulp-util');
 const fs = require('fs');
-var path = require('path');
+const path = require('path');
+const replace = require('gulp-replace');
+const rename = require("gulp-rename");
+const watch = require('gulp-watch');
+
 let reload_count = 0;
 
 var args = process.argv.slice(2);
 
 gulp.task('server', function() {
 
+  gulp.start('build');
+
   browserSync({
     server: {
-      baseDir: './src',
+      baseDir: './',
     },
     startPath: '/',
     injectChanges: true,
     open: false
   });
 
-  gulp.watch(['./src/**/*']).on('change', function(){
+  gulp.watch(['./bundle.js', './css/main.css', "./index.html"]).on('change', refrash);
+  watch('./css/**/*.css', refrash);
 
-    reload_count++;
-    gutil.log("Reloaded " + gutil.colors.magenta(reload_count) + ' times');
-    reload();
+
+  watch('./js/**/*.js', function () {
+
+    gulp.start('build');
 
   });
+
+});
+
+function refrash(){
+
+  reload_count++;
+  gutil.log("Reloaded " + gutil.colors.magenta(reload_count) + ' times');
+  reload();
+
+}
+
+gulp.task('build', function() {
+
+  gulp.src('./js/main.js')
+    .pipe(replace(/\/\/js: (.*)/g, function(match) {
+
+      let regex = /\/\/js: (.*)/;
+      let result = regex.exec(match);
+
+      let contents = "//" + result[1] + " not found";
+
+      if(fs.existsSync(result[1])){
+
+        contents = fs.readFileSync(result[1], 'utf8');
+
+      } else {
+
+        console.log(contents);
+
+      }
+
+      return contents;
+
+    }))
+    .pipe(rename("bundle.js"))
+    .pipe(gulp.dest('./'));
 
 });
 
@@ -36,11 +80,15 @@ if(args[0] == "create"){
   if (!fs.existsSync(args[1])){
 
     gulp.src(path.dirname(fs.realpathSync(__filename)) + "/empty/**/*")
-      .pipe(gulp.dest(args[1] + "/src"))
+      .pipe(gulp.dest(args[1]))
 
   } else {
     console.log(args[1] + " already exists");
   }
+
+} else if(args[0] == "build"){
+
+  gulp.start('build');
 
 } else {
 
